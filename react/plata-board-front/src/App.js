c51322ae44e123b81b208f4c4cb09778abe26241
platanus <platanus@canxan.com>
2022-08-13 15:08:31 +0900

    add: Split component

--- react/plata-board-front/src/App.js
+++ react/plata-board-front/src/App.js
@@ -1,101 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./component/page/HomePage";
+import BoardPage from "./component/page/BoardPage";
 
-// 파라미터를 보내는 첫번째 방법
-const getArticles = async () => {
-  await axios
-    .get(`http://localhost:3000/api/v1/board/1`, { params: { page: 2 } })
-    .then((response) => {
-      console.log("성공");
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log("실패");
-      console.log(error.response);
-    });
-};
+function App() {
+  return (
+	<BrowserRouter>
+	  <HomePage />
+      <Routes>
+        {/*<Route path="/*" element={<HomePage />} />*/}
+        <Route path="/board" element={<BoardPage />} />
+        <Route path="/board/:asdf" element={<BoardPage />} />
+      </Routes>
+	</BrowserRouter>
+  );
+}
 
-// App.js
-
-// 첫번째 방법
-const MyButton = ( props ) => {
-	const clickEvent = () => {
-	  console.log(props.clickMessage);
-	};
-	return <button onClick={clickEvent}>{props.buttonName}</button>;
-  };
-
-// App.js
-
-const MyButton2 = () => {
-	let countButton2 = 0;
-	const clickEvent = () => {
-	  countButton2 = countButton2 + 1;
-	  console.log(`countButton2: ${countButton2}`);
-	};
-	return <button onClick={clickEvent}>countButton2: {countButton2}</button>;
-  };
-  
-  const MyButton3 = () => {
-	const [countButton3, setcountButton3] = useState(0);
-	useEffect(() => {
-		console.log(`countButton3: ${countButton3}`);
-	  }, [countButton3]);
-	const clickEvent = () => {
-	  setcountButton3(countButton3 + 1);
-	  // console.log(`countButton3: ${countButton3}`);
-	};
-	return <button onClick={clickEvent}>countButton3: {countButton3}</button>;
-  };
-
-
-  const JsonBoard = () => {
-	const [boardInfo, setBoardInfo] = useState(null);
-	const [articlesInfo, setArticlesInfo] = useState(null);
-  
-	const getBoard = async () => {
-	  await axios
-		.get(`http://localhost:3000/api/v1/board/1`, { params: { page: 2 } })
-		.then((response) => {
-		  console.log("성공");
-		  console.log(response.data);
-  
-		  const { boardId, boardName, articles } = response.data;
-		  setBoardInfo({ boardId: boardId, boardName: boardName });
-		  setArticlesInfo(articles);
-		})
-		.catch((error) => {
-		  console.log("실패");
-		  console.log(error.response);
-		});
-	};
-  
-	useEffect(() => {
-	  getBoard();
-	}, []);
-  
-	return (
-	  <>
-			<div>{JSON.stringify(boardInfo)}</div>
-			<br />
-			<div>{JSON.stringify(articlesInfo?.content)}</div>
-	  </>
-	);
-  };
-
-
-  function App() {
-	//getArticles();
-	return (
-	  <>
-		<h1>Hello World</h1>
-		<MyButton buttonName={"ONE"} clickMessage={"HELLO"} />
-		<MyButton buttonName={"two"} clickMessage={"hi"} />
-		<MyButton2 buttonName={"two"} clickMessage={"hi"} />
-		<MyButton3 buttonName={"two"} clickMessage={"hi"} />
-		<JsonBoard />
-	  </>
-	);
-  }
-  
-  export default App;
+export default App;
---
+++ react/plata-board-front/src/component/page/BoardPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BoardTitle from "../board/BoardTitle";
+import Articles from "../board/Articles";
+import Pagenation from "../utils/Pagenation";
+import { useLocation, useParams } from "react-router-dom";
+
+const BoardPage = () => {
+  const [boardInfo, setBoardInfo] = useState(null);
+  const [articlesInfo, setArticlesInfo] = useState(null);
+  const [boardPageNum, setBoardPageNum] = useState(1);
+  const location = useLocation();
+  const match = useParams();
+
+  const getBoard = async (pageNum) => {
+    await axios
+      .get(`http://localhost:3000/api/v1/board/1`, {
+        params: { page: pageNum },
+      })
+      .then((response) => {
+        console.log(`request page: ${pageNum}`);
+        console.log(response.data);
+
+        const { boardId, boardName, articles } = response.data;
+        setBoardInfo({ boardId: boardId, boardName: boardName });
+        setArticlesInfo(articles);
+      })
+      .catch((error) => {
+        console.log("실패");
+        console.log(error.response);
+      });
+  };
+
+  useEffect(() => {
+    getBoard(boardPageNum);
+  }, [boardPageNum]);
+
+  useEffect(() => {
+    console.log(match);
+    console.log(location);
+  }, [match, location]);
+
+  return (
+    <>
+      <BoardTitle boardInfo={boardInfo} />
+      <Pagenation selectedPage={boardPageNum} setPage={setBoardPageNum} />
+      <Articles articlesInfo={articlesInfo} />
+    </>
+  );
+};
+
+export default BoardPage;
